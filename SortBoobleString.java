be48de4527e3ecaf5947ffbf0f9981eb32eea9c6
kononov_ay <aykononov@yandex.ru>
2020-09-16 13:06:19 +0300

    .

--- SortBoobleString.java
+++ SortBoobleString.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 
 class SortBoobleString {
     // Исходный массив
-    static private String[] strs = {"Now", "is", "the", "time", "for", "all",
+    private static String[] strs = {"Now", "is", "the", "time", "for", "all",
             "good", "men", "to", "come", "to", "the",
             "aid", "of", "their", "country"};
 
     // Метод Пузырьковой сортировки строк с учетом регистра compareTo()
-    static private String[] getSort(String[] str) {
+    private static String[] getSort(String[] str) {
         for (int i = 0; i < str.length; i++) {
             for (int j = i + 1; j < str.length; j++) {
                 if (str[j].compareTo(str[i]) < 0) {
